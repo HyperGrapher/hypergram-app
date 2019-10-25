d8fa148a56faaca83656bd4ee42445df1434157c
Burak Mestan <burakmestan@gmail.com>
2019-10-26 01:59:42 +0300

    Bottomnav first setup

--- app/src/main/java/com/modernkod/instaroid/MainActivity.kt
+++ app/src/main/java/com/modernkod/instaroid/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.modernkod.instaroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.modernkod.instaroid.views.CurvedBottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val curvedBottomNavigationView =
+            findViewById<CurvedBottomNavigationView>(R.id.bottomNavBar)
+        curvedBottomNavigationView.inflateMenu(R.menu.bottom_menu)
     }
 }
